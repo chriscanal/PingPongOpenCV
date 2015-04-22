700a47974a6cddbd66bc2cfe8a318e86fff7140b
Zane Gavin <zane.gavin@live.com>
2015-04-22 15:23:46 -0400

    OpenCV <3 cpp hough file

--- houghcircles (2).cpp
+++ houghcircles (2).cpp
@@ -1,7 +1,7 @@
-#include "opencv2/imgcodecs.hpp"
+//http://docs.opencv.org/doc/tutorials/imgproc/imgtrans/hough_circle/hough_circle.html <<look through
+//Taken from OpenCV samples
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-
 #include <iostream>
 
 using namespace cv;
@@ -11,12 +11,12 @@ static void help()
 {
     cout << "\nThis program demonstrates circle finding with the Hough transform.\n"
             "Usage:\n"
-            "./houghcircles <image_name>, Default is ../data/board.jpg\n" << endl;
+            "./houghcircles <image_name>, Default is pic1.png\n" << endl;
 }
 
 int main(int argc, char** argv)
 {
-    const char* filename = argc >= 2 ? argv[1] : "../data/board.jpg";
+    const char* filename = argc >= 2 ? argv[1] : "board.jpg";
 
     Mat img = imread(filename, 0);
     if(img.empty())
@@ -25,28 +25,26 @@ int main(int argc, char** argv)
         cout << "can not open " << filename << endl;
         return -1;
     }
-	
-	Mat cimg;
-	
+
+    Mat cimg;
     medianBlur(img, img, 5);
     cvtColor(img, cimg, COLOR_GRAY2BGR);
 
     vector<Vec3f> circles;
-    HoughCircles(img, circles, HOUGH_GRADIENT, 1, 10,
+    HoughCircles(img, circles, CV_HOUGH_GRADIENT, 1, 10,
                  100, 30, 1, 90 // change the last two parameters
                                 // (min_radius & max_radius) to detect larger circles
                  );
     for( size_t i = 0; i < circles.size(); i++ )
     {
         Vec3i c = circles[i];
-        circle( cimg, Point(c[0], c[1]), c[2], Scalar(0,0,255), 3, LINE_AA);
-        circle( cimg, Point(c[0], c[1]), 2, Scalar(0,255,0), 3, LINE_AA);
+        circle( cimg, Point(c[0], c[1]), c[2], Scalar(0,0,255), 3, CV_AA);
+        circle( cimg, Point(c[0], c[1]), 2, Scalar(0,255,0), 3, CV_AA);
     }
 
 	Size size(640, 480);
 	Mat outputImg;
 	resize(cimg, outputImg, size);
-
 
     imshow("detected circles", outputImg);
     waitKey();
