91fd029d5775dea0923bbf4f1e29d865508a15b3
Chris Canal on Macbook Air <christophercanal4@gmail.com>
2016-07-22 15:58:15 -0400

    fixed script

--- installDp.sh
+++ installDp.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-sudo apt-get install python-dev
-
-sudo apt-get install python-numpy python-scipy
-python-matplotlib python-pandas python-nose
-
-sudo apt-get install build-essential cmake pkg-config
-
-sudo apt-get install default-jdk ant
-
-sudo apt-get install libgtkglext1-dev
-
-sudo apt-get install bison
-
-sudo qt4-dev-tools libqt4-dev libqt4-core libqt4-gui
-
-sudo apt-get install v4l-utils
-
-sudo apt-get install qtcreator
+sudo apt-get install python-dev -y
+sudo apt-get install python-numpy -y
+sudo apt-get install python-nose -y
+sudo apt-get install python-pandas -y
+sudo apt-get install python-matplotlib -y
+sudo apt-get install python-scipy -y
+sudo apt-get install build-essential cmake pkg-config -y
+sudo apt-get install default-jdk ant -y
+sudo apt-get install libgtkglext1-dev -y
+sudo apt-get install bison -y
+sudo qt4-dev-tools libqt4-dev libqt4-core libqt4-gui -y
+sudo apt-get install v4l-utils -y
+sudo apt-get install qtcreator -y
